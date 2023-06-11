076eb60ee0de54c6be47043374022f976b5b261c
Mr-Hera <randellhera@gmail.com>
2023-06-12 01:29:32 +0300

    Feat: implemented validation to register form

--- market/forms.py
+++ market/forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Length, EqualTo, Email, DataRequired
 
 class RegisterForm(FlaskForm):
-    username = StringField(label='USERNAME:')
-    email_address = StringField(label='EMAIL:')
-    password1 = PasswordField(label='PASSWORD:')
-    password2 = PasswordField(label='CONFIRM PASSWORD:')
+    username = StringField(label='USERNAME:', validators=[Length(min=2, max=30), DataRequired()])
+    email_address = StringField(label='EMAIL:', validators=[Email(), DataRequired()])
+    password1 = PasswordField(label='PASSWORD:', validators=[Length(min=6), DataRequired()])
+    password2 = PasswordField(label='CONFIRM PASSWORD:', validators=[EqualTo('password1')])
     submit = SubmitField(label='Create Account')
